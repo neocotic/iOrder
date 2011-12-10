1ea744a9375d302d4d0af501f179cc718e882ded
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-12-10 18:53:31 +0000

    changed license to MIT and updated homepage

--- src/js/background.js
+++ src/js/background.js
@@ -1,25 +1,28 @@
-/* Copyright 2011 Alasdair Mercer
+/* Copyright (C) 2011 Alasdair Mercer, http://neocotic.com/
  * 
- * This file is part of iOrder.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  
+ * of this software and associated documentation files (the "Software"), to deal  
+ * in the Software without restriction, including without limitation the rights  
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
+ * copies of the Software, and to permit persons to whom the Software is  
+ * furnished to do so, subject to the following conditions:
  * 
- * iOrder is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The above copyright notice and this permission notice shall be included in all  
+ * copies or substantial portions of the Software.
  * 
- * iOrder is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with iOrder. If not, see http://www.gnu.org/licenses/.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  
+ * SOFTWARE.
  */
 
 /**
  * <p>Main controller for the extension and manages all interactions and
  * requests.</p>
- * @author <a href="http://github.com/neocotic">Alasdair Mercer</a>
+ * @author <a href="http://neocotic.com">Alasdair Mercer</a>
  * @since 1.0.0
  * @requires jQuery
  * @namespace
